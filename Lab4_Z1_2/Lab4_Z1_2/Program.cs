93c8c6d9c37c054f23b4af26b33940316f860656
a-lisek <anwrona@student.agh.edu.pl>
2019-11-17 17:08:28 +0100

    Lab4_Z1/2

---
+++ Lab4_Z1_2/Lab4_Z1_2/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Lab4_Z1_2
+{
+    public class Point
+    {
+        public int x, y;
+        public Point(int p1, int p2)
+        {
+            x = p1;
+            y = p2;
+        }
+    }
+
+    class MainClass
+    {
+
+        static void Fun1(in int i)
+        {
+            //i = i + 3;
+        }
+        static void Fun2(out int i)
+        {
+            i = 2;
+            i = i + 3;
+        }
+        static void Fun3(ref int i)
+        {
+            i = i + 3;
+        }
+        static void Fun4(int i)
+        {
+            i = i + 3;
+        }
+        static void Fun5(Point p)
+        {
+            Point p1 = new Point(4, 3);
+            p = p1;
+        }
+        static void Fun6(ref Point p)
+        {
+            Point p1 = new Point(4, 3);
+            p = p1;
+        }
+        public static void Main(string[] args)
+        {
+            int x = 4;
+
+            Fun1(x);
+            Console.WriteLine(x);
+            Fun2(out x);
+            Console.WriteLine(x);
+            Fun3(ref x);
+            Console.WriteLine(x);
+            Fun4(x);
+            Console.WriteLine(x);
+
+            short y = 3;
+            Fun1(y);
+            Console.WriteLine(y);
+            //Fun2(out y);
+            //Console.WriteLine(y);
+            //Fun3(ref y);
+           // Console.WriteLine(y);
+            Fun4(y);
+            Console.WriteLine(y);
+
+            Point pun = new Point(2, 1);
+
+            Console.WriteLine("{0},{1}", pun.x, pun.y);
+            Fun5(pun);
+            Console.WriteLine("{0},{1}",pun.x,pun.y);
+            Fun6(ref pun);
+            Console.WriteLine("{0},{1}", pun.x, pun.y);
+
+            pun = null;
+            //Fun5(pun);
+           // Console.WriteLine("{0},{1}", pun.x, pun.y);
+            pun = null;
+            Fun6(ref pun);
+            Console.WriteLine("{0},{1}", pun.x, pun.y);
+
+
+            //Zadanie 2
+
+        }
+
+    }
+}
---
+++ Lab4_Z1_2/Lab4_Z1_2/Zadanie2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Lab4_Z1_2
+{
+    class TestPointer
+    {
+        public unsafe void Cosik()
+        {
+            int[] list = { 10, 100, 200 };
+            fixed (int* ptr = list)
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.WriteLine("Adres [{0}]={1}", i, (int)(ptr + i));
+                    Console.WriteLine("Wartość[{0}]={1}", i, *(ptr + i));
+                }
+            Console.ReadKey();
+        }
+    }
+    public class Zadanie2
+    {
+        public unsafe void swap(int* p, int* q)
+        {
+            int temp = *p;
+            *p = *q;
+            *q = temp;
+        }
+            public Zadanie2()
+        {
+            TestPointer t = new TestPointer();
+            t.Cosik();
+        }
+    }
+}
